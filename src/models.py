68977038d2ad5cd4f0b82b5599edabf8133a83f9
cdavis2223 <cdavis2223@gmail.com>
2024-01-22 03:38:06 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -8,14 +8,36 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'person'
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Comment(Base):
+    __tablename__ = 'Comment'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'Post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'Media'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Follower(Base):
+    __tablename__ = 'Follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
